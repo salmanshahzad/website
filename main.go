245d6cfb2d6b3bcff18d27dede5d410263592316
Salman Shahzad <salshahzad@outlook.com>
2023-08-19 22:43:01 -0400

    Embed public files

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net"
 	"net/http"
 	"os"
 )
 
+//go:embed public
+var public embed.FS
+
 func main() {
-	addr := net.JoinHostPort("0.0.0.0", os.Getenv("PORT"))
-	if err := http.ListenAndServe(addr, http.FileServer(http.Dir("public"))); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is required")
+	}
+	addr := net.JoinHostPort(net.IPv4zero.String(), port)
+	public, err := fs.Sub(public, "public")
+	if err != nil {
+		log.Fatalf("Could not find public directory: %v\n", err)
+	}
+	if err := http.ListenAndServe(addr, http.FileServer(http.FS(public))); err != nil {
+		log.Fatalf("Could not start server: %v", err)
 	}
 }
